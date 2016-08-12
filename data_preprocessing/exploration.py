48b3a5d15b1712c1238ae5f7cb0136dbab6a77a3
Carolina Arias Muñoz <krolinarias@gmail.com>
2016-08-12 03:22:48 +0200

    Update exploration.py

--- data_preprocessing/exploration.py
+++ data_preprocessing/exploration.py
@@ -15,65 +15,116 @@ Python version: 2.7
 
 import pandas
 import numpy
+import matplotlib
+matplotlib.style.use('ggplot')
 import scipy
 from scipy import stats
 
 
-data_path = '/media/sf_2_PhD_2013_-2014/1PhD_WorkDocs/PhD_Data-calculations/data/wundermap/nov_dic_2013/csv/'
+
+data_path = 'your path'
 
 #Importing data
-callsout = pandas.read_table('/media/sf_2_PhD_2013_-2014/1PhD_WorkDocs/PhD_Data-calculations/data/sms-call-internet-mi/ts_cellid_variable/calls_out.txt', names=['date_time','cellid','calls_out'], header=None )
+callsout = pandas.read_table(data_path + 'callsout_dic.tsv', sep='\t', names=['date_time','cellid','calls_out'], header=None )
 
 # Setting date_time as data frame index
 callsout = callsout.sort(columns='date_time', axis=0, ascending=True)
 callsout = callsout.reset_index(drop=True)
 
-#-date time index
-rng = pandas.date_range(start='2013-10-31 23:00:00', periods=1488, freq='H')
 #some info on the data
 callsout.info()
 
-#creating temporal indexes
+#creating time index
 callsout = callsout.set_index(pandas.DatetimeIndex(callsout['date_time']))
 
 #------------------------------#
 # STATISTICS AND PLOTS
 #------------------------------#
-statistics = callsout.describe()
-stats.to_csv(path_or_buf = data_path + 'datamergedstats.csv', sep=',')
-lineplot = callsout['calls_out'].plot(kind='density',logy=False, figsize=(20,5))
-histplot = callsout['calls_out'].plot(kind='hist',logy=False, figsize=(20,5))
-#histplot2 = callsout.hist('calls_out', bins=500, figsize=(20,5))
-boxplot = callsout['calls_out'].plot(kind='box',logy=False, figsize=(20,5))
-histplot = callsout['calls_out'].plot(kind='line',logy=False, figsize=(20,5))
+#calculate statistics
+statistics = callsout['calls_out'].describe()
+#count    7.677681e+06
+#mean     1.322562e+00
+#std      2.175737e+00
+#min     -1.296900e+01
+#25%     -6.975566e-02
+#50%      1.626751e+00
+#75%      2.936448e+00
+#max      7.860609e+00
+#Name: calls_out, dtype: float64
+
+#saving the statistics in a csv file
+statistics.to_csv(data_path + 'callsoutstats.csv', sep=',')
+#simple plot
+line = callsout['calls_out'].plot(kind='line',logy=False, title = 'Outgoing calls Dic 2013', figsize=(20,5))
+#density plot
+density = callsout['calls_out'].plot(kind='density',logy=False, title = 'Outgoing calls Dic 2013. Density Plot')
+#histogram
+histplot = callsout['calls_out'].plot(kind='hist',logy=False,title = 'Outgoing calls Dic 2013. Histogram') 
+#boxplot
+boxplot = callsout['calls_out'].plot(kind='box',logy=False,title = 'Outgoing calls Dic 2013. Boxplot')
+#dataframe to series
 
 #------------------------------#
 # NORMALIZING DATA
 #------------------------------#
-
-#setting values cero to NAN 
+#setting values cero to 0.1: log (0) = undefined
 callsout = callsout.replace(0, 0.1)
-calloutlog = callsout['callsout']
 #callsout = callsout.replace(0, numpy.nan)
 
-#-Transforming data into log 
-callsout['calls_out']= numpy.log(callsout['calls_out'])
+#Transforming data into log 
+callsoutlog = numpy.log(callsout['calls_out'])
+callsout['calls_out'] = callsoutlog
+#Cheking normality
+histplot = callsout['calls_out'].plot(kind='hist',logy=False, title = 'Outgoing calls Dic 2013 Normalized [log(x)]. Histogram')
 
-#Normality test
-#histogram to check normality
-histplot = callsout.plot(kind='hist',logy=False, figsize=(20,5))
-#boxplot to check for outliers
-boxplot = callsout.plot(kind='box',logy=False, figsize=(20,5))
+#---------------------------------------------------#
+# Cheking OUTLIERS 
+#---------------------------------------------------#
+#standard deviaton of data
+std = callsout['calls_out'].std()
+#Interquartile range
+iqr = 1.35 * std
+#25% quartile
+q1 = statistics.ix['25%']
+#75% quartile
+q3 = statistics.ix['75%']
+#inferior outer fence limit
+inf_out_lim = q1 - (3 * iqr)
+#superior outer fence limit
+sup_out_lim = q3 + (3 * iqr)
 
-scipy.stats.normaltest(callsout, axis=0)
-#NormaltestResult(statistic=803869.62940951809, pvalue=0.0)
-scipy.stats.shapiro(callsout, a=None, reta=False)
-#(0.9956305623054504, 0.0)
-scipy.stats.anderson(callsout, dist='norm')
-#AndersonResult(statistic=87569.946015860885, critical_values=array([ 0.576,  0.656,  0.787,  0.918,  1.092]), significance_level=array([ 15. ,  10. ,   5. ,   2.5,   1. ]))
-#AndersonResult(statistic=0.27488219365477562, critical_values=array([ 0.576,  0.656,  0.787,  0.918,  1.092]), significance_level=array([ 15. ,  10. ,   5. ,   2.5,   1. ]))
-scipy.stats.t.fit(callsout)
+#detecting mayor outliers
+callsout['i_outliers'] = callsout['calls_out'].apply(lambda x: 'inf_major_outlier' if x<inf_out_lim else 'NI')
+callsout['s_outliers'] = callsout['calls_out'].apply(lambda x: 'sup_major_outlier' if x>sup_out_lim else 'NI')
 
+#pivot table to count the number of ouliers
+pandas.pivot_table(callsout, values='calls_out', columns=['i_outliers', 's_outliers'], aggfunc=numpy.count_nonzero)
+#i_outliers         s_outliers
+#NI                 NI            7676044.0
+#inf_major_outlier  NI               1637.0
+#dtype: float64
+#------------------------------------------
+#no sup outliers cause the max value is < sup = 11.74 . inf_major_outlier = 1637
+callsout.drop(['i_outliers','s_outliers'],inplace=True,axis=1) 
+
+#create a new dataset without the outliers
+callsout['callsout_out'] = callsout['calls_out'].apply(lambda x: numpy.nan if x<inf_out_lim else x)
+#check the new dataset
+callsout['callsout_out'].describe()
+#count    7.676044e+06
+#mean     1.324923e+00
+#std      2.169920e+00
+#min     -8.880377e+00
+#25%               NaN
+#50%               NaN
+#75%               NaN
+#max      7.860609e+00
+#Name: callsout_out, dtype: float64
+histout = callsout['callsout_out'].plot(kind='hist',logy=False, title = 'Outgoing calls Dic 2013 Normalized [log(x)] and no outliers. Histogram')
+lineout = callsout['callsout_out'].plot(kind='line',logy=False, title = 'Outgoing calls Dic 2013 Normalized [log(x)] and no outliers')
+
+#saving file into a csv
 callsout.to_csv(path_or_buf = data_path + 'callout_normal_dic.csv', sep=',')
+
 
 print 'enjoy! bye'
