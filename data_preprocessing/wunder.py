5f369503fb66743034288f205106568940478629
Carolina Arias Muñoz <krolinarias@gmail.com>
2016-08-12 03:25:10 +0200

    Update wunder.py

--- data_preprocessing/wunder.py
+++ data_preprocessing/wunder.py
@@ -14,14 +14,11 @@ Python version: 2.7
 ------------------------------------------------------------------------------------------------------------------
 """
 import pandas
-import matplotlib.pyplot as plt
 import numpy
-import scipy
-import csv, numpy, time
 import os
 import glob
-from fitter import Fitter
-from scipy import stats
+import matplotlib
+matplotlib.style.use('ggplot')
 
 #------------------------------#
 # glosary
@@ -45,13 +42,14 @@ from scipy import stats
 #------------------------------#
 
 #path to original csv files
-data_path = '/media/sf_2_PhD_2013_-2014/1PhD_WorkDocs/PhD_Data-calculations/data/wundermap/nov_dic_2013/csv/'
+data_path = 'your path'
 #path for csv files aggregated by hour
 data_path_aggregated = data_path + 'aggregated/'
 data_path_nov = data_path_aggregated + 'Nov/'
 data_path_dic = data_path_aggregated + 'Dic/'
 #path for final big csv file
 data_path_out = data_path + 'out/'
+
 data_files = glob.glob(data_path + '*.csv')
 
 if not os.path.exists(data_path_aggregated):
@@ -72,7 +70,7 @@ for data_file in data_files:
         #extracting the station name from the data, it is useful to name the output csv files 
         stationcolumn = rawdata['name']
         stationname = stationcolumn[1]
-        #Data Cleaning: eliminating the extrange values -573, and others from data
+        #Data Cleaning: eliminating the strange values -573, and others from data
         rawdata.loc[rawdata['tmp'] < -20, 'tmp'] = numpy.nan
         #Data Cleaning: eliminating the negative values of precipitation from data
         rawdata.loc[rawdata['prec'] < 0, 'prec'] = numpy.nan
@@ -100,37 +98,55 @@ with open(data_path_out + 'data_dic.csv', 'a') as out_file:
             line_num = line_num + 1
         print line_num
     
+    
+#------------------------------#
+# GPLOTS
+#------------------------------# 
+ 
+data_files = glob.glob(data_path_dic + '*.csv')
+   
+for data_file in data_files:
+        
+    with open(data_file, 'rb') as data_file:
+         
+        #converting csv file into a dataframe          
+        stationdata = pandas.read_table(data_file, sep=',', names=['ts','tmp','prec','name'], header=0)         
+        #extracting the station name from the data, it is useful to name the output csv files 
+        stationcolumn = stationdata['name']
+        stationname = stationcolumn[1]
+        # set the datetime as index
+        stationdata = stationdata.set_index(pandas.DatetimeIndex(stationdata['ts'])) 
+        # plot
+        stationdata.plot(x='ts', y='tmp', figsize=(18,5), title= stationname) 
+    
 #------------------------------#
 # DATA MAPPING INTO MILANO GRID
 #------------------------------#
 # importing the table that relates cell id from milano grid to the stations data
-cellid = pandas.read_table('/media/sf_2_PhD_2013_-2014/1PhD_WorkDocs/PhD_Data-calculations/data/wundermap/nov_dic_2013/cellid_wunder2013stations.csv',sep=',', names=['cellid','latcellid','longcellid','latst','longst','name'], header=0 )
+cellid = pandas.read_table(data_path + 'cellid_wunder2013stations.csv',sep=',', names=['cellid','latcellid','longcellid','latst','longst','name'], header=0 )
 # importing the data
 data = pandas.read_table(data_path_out + 'data_dic.csv', sep=',', names=['ts','tmp','prec','name'], header=0)
+
+#------prepare data to plot-------#
+#sort by time stamp
+data = data.sort(columns='ts', axis=0, ascending=True)
+#reset indet to create new one
+data = data.reset_index(drop=True)
+#creating time index
+data = data.set_index(pandas.DatetimeIndex(data['ts']))
+
+#histogram
+histplot = data['tmp'].plot(kind='hist',logy=False, figsize=(20,5))
+#histplot2 = callsout.hist('calls_out', bins=500, figsize=(20,5))
+#boxplot
+boxplot = data['tmp'].plot(kind='box',logy=False)
+#dataframe to series
+tmp = data['tmp']
+
 # merging the two data frames
 datamerged = cellid.merge(data, on='name')
 # leave just 
 datamerged.drop(['prec','latcellid','longcellid','latst','longst'],inplace=True,axis=1) 
 # save merged data into a csv          
 datamerged.to_csv(path_or_buf = data_path_out + 'datamerged_dic.csv', sep=',')
-
-
-#------------------------------#
-# DATA SUBSETTING BY TIME
-#------------------------------#
-# set the timestamp as index
-datamerged = datamerged.set_index(pandas.DatetimeIndex(datamerged['ts']))#it takes at least 30 min to execute
-datamerged_1ts = datamerged.ix['2013-12-31 23:00:00']
-
-
-
-#------------------------------#
-# STATISTICS
-#------------------------------#
-
-stats = datamerged.describe()
-stats.to_csv(path_or_buf = data_path_aggregated + 'datamergedstats.csv', sep=',')
-lineplot = datamerged['tmp'].plot(kind='scatter',logy=False, figsize=(20,5))
-histplot = datamerged['tmp'].plot(kind='hist',logy=False, figsize=(20,5))
-boxplot = datamerged['tmp'].plot(kind='box',logy=False, figsize=(20,5))
 
